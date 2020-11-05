399b0ba8b72cd334ee711db1b233599544fbb113
Rosano Coutinho <pub@xeari.com>
2020-11-05 11:08:24 -0300

    Skip post install if no node_modules

--- package-postinstall.js
+++ package-postinstall.js
@@ -1,23 +1,43 @@
-(function OLSKHotfixLaunchletForSkipWait() {
-	const filePath = './node_modules/launchlet/__compiled/launchlet.js';
-	require('fs').writeFileSync(filePath, require('OLSKString').OLSKStringPatch(
-		require('fs').readFileSync(filePath, 'utf8'),
-		',100',
-		',1',
-	));
-})();
+const mod = {
 
-(function OLSKHotfixModulesForWindow() {
-	[
-		'OLSKPact',
-		'OLSKCrypto',
-	].map(function (e) {
-		const filePath = `./node_modules/${ e }/main.js`;
-		require('fs').writeFileSync(filePath, require('fs').readFileSync(filePath, 'utf8').replace(/\bmod\b/g, e));
-	})
-})();
+	// SETUP
 
-(function OLSKHotfixOLSKCryptoForRequire() {
-	const filePath = './node_modules/OLSKCrypto/main.js';
-	require('fs').writeFileSync(filePath, require('fs').readFileSync(filePath, 'utf8').replace("const openpgp = require('openpgp');", "const openpgp = (function() { return typeof require === 'undefined' ? window.openpgp : require('openpgp'); })();"));
-})();
+	SetupEverything() {
+		if (!require('fs').existsSync(require('path').join(__dirname, 'node_modules'))) {
+			return false;
+		}
+		
+		(function OLSKHotfixLaunchletForSkipWait() {
+			const filePath = './node_modules/launchlet/__compiled/launchlet.js';
+			require('fs').writeFileSync(filePath, require('OLSKString').OLSKStringPatch(
+				require('fs').readFileSync(filePath, 'utf8'),
+				',100',
+				',1',
+			));
+		})();
+
+		(function OLSKHotfixModulesForWindow() {
+			[
+				'OLSKPact',
+				'OLSKCrypto',
+			].map(function (e) {
+				const filePath = `./node_modules/${ e }/main.js`;
+				require('fs').writeFileSync(filePath, require('fs').readFileSync(filePath, 'utf8').replace(/\bmod\b/g, e));
+			})
+		})();
+
+		(function OLSKHotfixOLSKCryptoForRequire() {
+			const filePath = './node_modules/OLSKCrypto/main.js';
+			require('fs').writeFileSync(filePath, require('fs').readFileSync(filePath, 'utf8').replace("const openpgp = require('openpgp');", "const openpgp = (function() { return typeof require === 'undefined' ? window.openpgp : require('openpgp'); })();"));
+		})();
+	},
+
+	// LIFECYCLE
+
+	LifecycleModuleDidLoad() {
+		mod.SetupEverything();
+	},
+
+};
+
+mod.LifecycleModuleDidLoad();
