83ad6a0a2748a1700ec77db43a515148feb9e0a4
Rosano Coutinho <pub@xeari.com>
2020-11-12 10:29:40 -0300

    Send encrypted payload

--- controller.js
+++ controller.js
@@ -15,7 +15,7 @@ const mod = {
 			OLSKRoutePath: '/OLSKFundStubGrantRoute',
 			OLSKRouteMethod: 'post',
 			OLSKRouteSignature: 'OLSKFundStubGrantRoute',
-			OLSKRouteFunction: function OLSKFundStubGrantRoute (req, res, next) {
+			OLSKRouteFunction: async function OLSKFundStubGrantRoute (req, res, next) {
 				if (req.body.OLSKFundStubGrantRoute === 'OLSKFundStubGrantRouteExpired') {
 					return res.json({
 						OLSKPactGrantEndDate: new Date(Date.now() - 1000),
@@ -23,7 +23,7 @@ const mod = {
 				}
 
 				return res.json({
-					OLSK_FUND_ENCRYPTED_SIGNED: 'bravo',
+					OLSK_FUND_ENCRYPTED_SIGNED: require('OLSKCrypto').OLSKCryptoEncryptSigned(process.env.OLSK_CRYPTO_PAIR_RECEIVER_PUBLIC, process.env.OLSK_CRYPTO_PAIR_SENDER_PRIVATE, JSON.stringify({})),
 				});
 			},
 		}];
